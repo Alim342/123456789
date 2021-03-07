f18f30f642c08332e48a112238db2c5a464793db
Темирболат Алим Даулетулы <@lcoast.itstep>
2021-03-07 11:11:41 +0600

    певый урок

---
+++ 111/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args) { 
+        {
+                //Console.WriteLine("Сколько вам лет ?");
+                //int age = Console.ToInt32(Console.ReadLine());
+
+                ////string s_age = Convert.ToString(age);
+
+
+                //Console.WriteLine("Ваш возраст:"+age);
+                //if (age > 18) {
+                //    Console.WriteLine("Вы болшой");
+
+                //int a = 0;
+                //while (a < 10) {
+                //    Console.WriteLine(a);
+                //        a++;
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(i);
+                }
+
+        }
+    }
+}
